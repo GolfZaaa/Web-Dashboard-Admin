2af271b1ef10f0beeb337ad99bc2289d052c9c28
GolfZaaa <you@example.com>
2023-07-12 18:27:18 +0700

    Start Website

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import HeaderPage from './components/screens/Header/HeaderPage';
+import FooterPage from './components/screens/Footer/FooterPage';
+import { Space } from 'antd';
+import SlideMenu from './components/slidemenu/SlideMenu';
+import ContentPage from './components/screens/Content/ContentPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <HeaderPage />
+      <Space>
+        <SlideMenu></SlideMenu>
+        <ContentPage></ContentPage>
+      </Space>
+      <FooterPage />
+    </Fragment>
   );
 }
 
---
+++ src/components/screens/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import '../css/style.css'; // นำเข้าไฟล์ CSS
+
+export default function Dashboard() {
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
